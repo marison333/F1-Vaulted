14cb3f3f5cbf03c5742633842189fe786835422e
marison333 <marison.sol@outlook.com>
2025-07-06 11:02:06 +0200

    feat(mock): create new grand-prix mock data

---
+++ src/mock/grand-prix.ts
@@ -0,0 +1,149 @@
+import { GrandPrix } from '@/types';
+
+const grandPrix: GrandPrix[] = [
+    {
+        id: 'austria-gp',
+        laps: 70,
+        name: 'FORMULA 1 MSC CRUISES AUSTRIAN GRAND PRIX 2025',
+        location: {
+            nationality: {
+                code: 'AT',
+                name: 'Austrian',
+                iconUrl: '/icons/flags/at.svg'
+            },
+            circuit: 'Red Bull Ring, Spielberg'
+        }
+    },
+    {
+        id: 'canada-gp',
+        laps: 70,
+        name: 'FORMULA 1 PIRELLI GRAND PRIX DU CANADA 2025',
+        location: {
+            nationality: {
+                code: 'CA',
+                name: 'Canadian',
+                iconUrl: '/icons/flags/ca.svg'
+            },
+            circuit: 'Circuit Gilles Villeneuve, Montreal'
+        }
+    },
+    {
+        id: 'spain-gp',
+        laps: 66,
+        name: 'FORMULA 1 GRAN PREMIO DE ESPAÑA 2025',
+        location: {
+            nationality: {
+                code: 'ES',
+                name: 'Spanish',
+                iconUrl: '/icons/flags/es.svg'
+            },
+            circuit: 'Circuit de Barcelona-Catalunya, Barcelona'
+        }
+    },
+    {
+        id: 'monaco-gp',
+        laps: 78,
+        name: 'FORMULA 1 TAG HEUER GRAND PRIX DE MONACO 2025',
+        location: {
+            nationality: {
+                code: 'MC',
+                name: 'Monacan',
+                iconUrl: '/icons/flags/mc.svg'
+            },
+            circuit: 'Circuit de Monaco, Monte Carlo'
+        }
+    },
+    {
+        id: 'emilia-romagna-gp',
+        laps: 63,
+        name: `FORMULA 1 AWS GRAN PREMIO DEL MADE IN ITALY E DELL'EMILIA-ROMAGNA 2025`,
+        location: {
+            nationality: {
+                code: 'IT',
+                name: 'Italian',
+                iconUrl: '/icons/flags/it.svg'
+            },
+            circuit: 'Autodromo Internazionale Enzo e Dino Ferrari, Imola'
+        }
+    },
+    {
+        id: 'miami-gp',
+        laps: 57,
+        name: 'FORMULA 1 CRYPTO.COM MIAMI GRAND PRIX 2025',
+        location: {
+            nationality: {
+                code: 'US',
+                name: 'American',
+                iconUrl: '/icons/flags/us.svg'
+            },
+            circuit: 'Miami International Autodrome, Miami'
+        }
+    },
+    {
+        id: 'saudi-arabia-gp',
+        laps: 50,
+        name: 'FORMULA 1 STC SAUDI ARABIAN GRAND PRIX 2025',
+        location: {
+            nationality: {
+                code: 'SA',
+                name: 'Saudi Arabian',
+                iconUrl: '/icons/flags/sa.svg'
+            },
+            circuit: 'Jeddah Corniche Circuit, Jeddah'
+        }
+    },
+    {
+        id: 'bahrain-gp',
+        laps: 57,
+        name: 'FORMULA 1 GULF AIR BAHRAIN GRAND PRIX 2025',
+        location: {
+            nationality: {
+                code: 'BH',
+                name: 'Bahraini',
+                iconUrl: '/icons/flags/bh.svg'
+            },
+            circuit: 'Bahrain International Circuit, Sakhir'
+        }
+    },
+    {
+        id: 'japan-gp',
+        laps: 53,
+        name: 'FORMULA 1 LENOVO JAPANESE GRAND PRIX 2025',
+        location: {
+            nationality: {
+                code: 'JP',
+                name: 'Japanese',
+                iconUrl: '/icons/flags/jp.svg'
+            },
+            circuit: 'Suzuka Circuit, Suzuka'
+        }
+    },
+    {
+        id: 'china-gp',
+        laps: 56,
+        name: 'FORMULA 1 HEINEKEN CHINESE GRAND PRIX 2025',
+        location: {
+            nationality: {
+                code: 'CN',
+                name: 'Chinese',
+                iconUrl: '/icons/flags/cn.svg'
+            },
+            circuit: 'Shanghai International Circuit, Shanghai'
+        }
+    },
+    {
+        id: 'australia-gp',
+        laps: 58,
+        name: 'FORMULA 1 LOUIS VUITTON AUSTRALIAN GRAND PRIX 2025',
+        location: {
+            nationality: {
+                code: 'AU',
+                name: 'Australian',
+                iconUrl: '/icons/flags/au.svg'
+            },
+            circuit: 'Melbourne Grand Prix Circuit, Melbourne'
+        }
+    }
+];
+
+export default grandPrix;
