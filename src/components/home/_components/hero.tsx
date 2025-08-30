2f62817389fd2cd72ccab0dfcea2213c11cd0b2b
marison333 <marison.sol@outlook.com>
2025-08-30 13:26:35 +0200

    feat(hero): hero component fully responsive to all screen sizes

--- src/components/home/_components/hero.tsx
+++ src/components/home/_components/hero.tsx
@@ -3,44 +3,35 @@ import Image from 'next/image';
 
 import { Card } from '@/components/ui/card';
 
+const heroItems = [
+    { href: '/drivers', imageSrc: '/images/home/hero/driver.png', text: 'drivers' },
+    { href: '/results', imageSrc: '/images/home/hero/battle.png', text: 'results' }
+];
+
 export default function Hero() {
     return (
         <div
-            className='relative w-[90rem] h-[80vh] grid grid-cols-2 gap-1 [&>*]:border-zinc-950'
+            className='relative flex flex-wrap md:grid md:grid-cols-2 gap-2 px-1.5 mx-auto [&>*]:border-zinc-950'
             data-slot='hero'>
-            <Card className='relative flex col-span-1 overflow-hidden'>
-                <Link className='w-full' href='/current-grid'>
-                    <Image
-                        alt='Hero image of f1 driver lewis hamilton'
-                        className='object-cover w-full h-full'
-                        fill
-                        priority
-                        src='/images/home/hero/driver.png'
-                    />
-                    <div className='bg-gradient-to-b from-transparent to-black absolute z-20 bottom-0 left-0 right-0 p-4 w-full'>
-                        <span className='text-white text-2xl font-semibold'>
-                            view your favorite driver
-                        </span>
-                    </div>
+            {heroItems.map((item, index) => (
+                <Link key={item.href} href={item.href}>
+                    <Card className='relative h-[40vh] w-full md:h-[55vh] md:w-full mx-auto flex col-span-1 overflow-hidden'>
+                        <Image
+                            alt={`Hero image #${index + 1}`}
+                            className='relative object-cover md:object-top md:size-full'
+                            height={500}
+                            priority
+                            src={`${item.imageSrc}`}
+                            width={500}
+                        />
+                        <div className='bg-gradient-to-b from-transparent to-black absolute z-5 bottom-0 left-0 right-0 p-4 w-full'>
+                            <span className='text-white text-2xl font-semibold'>
+                                {`Find all 2025 ${item.text}`}
+                            </span>
+                        </div>
+                    </Card>
                 </Link>
-            </Card>
-            <Card className='relative flex col-span-1 overflow-hidden'>
-                <Link className='w-full' href='#'>
-                    {/* @TODO: Add link to standings */}
-                    <Image
-                        alt='Hero image of f1 driver lewis hamilton'
-                        className='object-cover w-full h-full'
-                        fill
-                        priority
-                        src='/images/home/hero/battle.png'
-                    />
-                    <div className='bg-gradient-to-b from-transparent to-black absolute z-20 bottom-0 left-0 right-0 p-4 w-full'>
-                        <span className='text-white text-2xl font-semibold'>
-                            view race results
-                        </span>
-                    </div>
-                </Link>
-            </Card>
+            ))}
         </div>
     );
 }
