a3449abb2e9ec92363d2354dfa4d162857d51116
marison333 <marison.sol@outlook.com>
2025-09-14 13:53:09 +0200

    feat(data): created new get driver name function

--- src/lib/data/drivers.ts
+++ src/lib/data/drivers.ts
@@ -4,3 +4,22 @@ import drivers from '@/mock/drivers';
 export async function getDrivers(): Promise<Driver[]> {
     return drivers;
 }
+
+export function getDriverByName(searchName: string): Promise<Driver> {
+    const sanitizedSearchName = searchName;
+
+    const driver = drivers.find((driver) => {
+        const fullName = `${driver.givenName} ${driver.familyName}`.toLowerCase();
+
+        return (
+            fullName === sanitizedSearchName.toLowerCase() ||
+            fullName.replace(/\s+/g, '-') === sanitizedSearchName
+        );
+    });
+
+    if (!driver) {
+        return Promise.reject(new Error(`Driver not found: ${searchName}`));
+    }
+
+    return Promise.resolve(driver);
+}
